b7e7161f94ef6c7034557cde69d379d36bf44604
Niko Böckerman <git.585wj@slmail.me>
2024-02-10 22:40:51 +0000

    Improve fish-persistent-data feature

---
+++ src/fish-persistent-data/onCreate.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+mount_point="/mnt/fish-persistent-data"
+user_local_dir=$HOME/.local
+user_share_dir=$user_local_dir/share
+user_fish_data_dir=$user_share_dir/fish
+
+# Migrate fish data from location used in versions 1.0.* to new location
+prev_fish_persistent_dir="$mount_point"/fish
+if [ -d "$prev_fish_persistent_dir" ]; then
+    echo "Migrating data from: $prev_fish_persistent_dir to $mount_point"
+    mv "$prev_fish_persistent_dir"/* "$mount_point"
+    rmdir "$prev_fish_persistent_dir"
+fi
+
+sudo() {
+    if [ "$USER" = root ]; then
+        "$@"
+    else
+        command sudo "$@"
+    fi
+}
+
+# Prepare mounted volume permissions
+sudo chown "$USER":"$USER" "$mount_point"
+
+# Rename theoretical old fish data directory
+if [ -d "$user_fish_data_dir" ]; then
+    mv "$user_fish_data_dir" "$user_fish_data_dir-old"
+fi
+
+# Create new fish data directory as symlink to the mounted volume
+mkdir -p "$user_share_dir"
+chown "$USER":"$USER" -R "$user_local_dir"
+ln -s "$mount_point" "$user_fish_data_dir"
+chown -h "$USER":"$USER" "$user_fish_data_dir"
